5884fd28c4563c41eebbda3a4c2b7a55da6d4d02
jhedgee <josh.brown1290.z@gmail.com>
2024-11-17 19:52:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -91,3 +91,140 @@ function displayScene(sceneKey) {
     buttonContainer.appendChild(button);
   });
 }
+
+
+
+// Scenes definition
+const scenes = {
+  titleScreen: {
+    text: `<h1>To Write or Not to Write: The Game</h1>
+           <h3>by Joshua Brown</h3>`,
+    options: [
+      { text: "Start", nextScene: "intro" }
+    ]
+  },
+  intro: {
+    text: `Hey there, player. Welcome to my story: The Game. 
+           You get to make decisions for me and I hope you enjoy it.
+           <br><br>
+           How do you want it to begin?`,
+    options: [
+      { text: "Positive Experience", nextScene: "positiveExperience" },
+      { text: "Negative Experience", nextScene: "negativeLore" }
+    ]
+  },
+  negativeLore: {
+    text: `As much as I loved going wild with my stories, there was always one huge issue... grammar.`,
+    options: [
+      { text: "Next", nextScene: "middleSchool" }
+    ]
+  },
+  middleSchool: {
+    text: `It's middle school and I finally have to do real work (surprised it took this long). I need to finish this essay ASAP, but it would be sooooo easy to slack off. Still, I really feel like it needs some work.`,
+    options: [
+      { text: "Revise with Dad", nextScene: "suffering" },
+      { text: "Turn it in", nextScene: "failure" }
+    ]
+  },
+  suffering: {
+    text: `This is brutal, he's reading it back to me and making marks all over the page. This sounds horrible, I made so many mistakes and feel like crying.`,
+    options: [
+      { text: "Next", nextScene: "success" }
+    ]
+  },
+  success: {
+    text: `Hey! I got an A-, I understand how to write better now but dang, that was a tough lesson to learn.`,
+    options: [
+      { text: "Restart", nextScene: "titleScreen" }
+    ]
+  },
+  failure: {
+    text: `OOOOOF, I sure hope the teacher goes easy on me.`,
+    options: [
+      { text: "Next", nextScene: "badGrade" }
+    ]
+  },
+  badGrade: {
+    text: `C+, such a horrible grade, I really wish I had prepared more...`,
+    options: [
+      { text: "Next", nextScene: "secondChance" }
+    ]
+  },
+  secondChance: {
+    text: `Another essay? Oh boy... Time to prepare (or not prepare).`,
+    options: [
+      { text: "Barely put forth an effort", nextScene: "rockbottom" },
+      { text: "Get creative then revise with Dad", nextScene: "winr2chckndinr" }
+    ]
+  },
+  rockbottom: {
+    text: `F?? Dinner time conversation is going to suuuck (and I'll probably get grounded).`,
+    options: [
+      { text: "Restart", nextScene: "titleScreen" }
+    ]
+  },
+  winr2chckndinr: {
+    text: `I am so proud of this one, I wrote what I wanted, then listened to my dad's criticism and made a masterpiece. I guess the key all along was to blend the right amount of creativity and punctuality.`,
+    options: [
+      { text: "Restart", nextScene: "titleScreen" }
+    ]
+  },
+  positiveExperience: {
+    text: `When I was a little kid I was OBSESSED with fantasy books. I loved to create stories and always dreamt of heroic characters defeating evil. One such story I made in elementary school. It had two characters, TacoCat and BurritoDog. What should I do with this story?`,
+    options: [
+      { text: "Show it to my mom and my barber", nextScene: "momstory" },
+      { text: "Hide it away in desk", nextScene: "desksad" }
+    ]
+  },
+  desksad: {
+    text: `Aw man, I'm sure people would have loved to hear that story... Great job stifling my story, player >:( Anyways, on to the next part.`,
+    options: [
+      { text: "Next", nextScene: "freewill" }
+    ]
+  },
+  momstory: {
+    text: `I know it's weird to bring a story to a barber but little me was so proud. Both my barber and my mom loved it. It was so great showcasing my creativity.`,
+    options: [
+      { text: "Next", nextScene: "freewill" }
+    ]
+  },
+  freewill: {
+    text: `I'm bored, what do you think I should do?`,
+    options: [
+      { text: "Paint", nextScene: "drawing" },
+      { text: "Play videogames", nextScene: "MLG" }
+    ]
+  },
+  MLG: {
+    text: `I played on my Xbox for the whole afternoon. I don't really feel much better after that. Maybe my brain is rotting away.`,
+    options: [
+      { text: "Next", nextScene: "sadending" }
+    ]
+  },
+  drawing: {
+    text: `Wow, that was fun, my mom even came over and we drew for hours and hours. I can feel creativity flowing through me (not literally, that would be weird).`,
+    options: [
+      { text: "Next", nextScene: "creativeending" }
+    ]
+  },
+  creativeending: {
+    text: `Nice job player, you've certainly made some decisions. I'm glad we got to end on a creative note. My mom has really helped me be more imaginative and I think the future is looking pretty bright.`,
+    options: [
+      { text: "Restart", nextScene: "titleScreen" }
+    ]
+  },
+  sadending: {
+    text: `C'mon player, I don't think those video games really helped me in the long run... Why don't you try tapping into your imagination next time?`,
+    options: [
+      { text: "Restart", nextScene: "titleScreen" }
+    ]
+  }
+};
+
+// Function to start the game from the title screen
+function startGame() {
+  displayScene("intro");
+}
+
+// Display the title screen when the page loads
+displayScene("titleScreen");
